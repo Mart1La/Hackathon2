cd6fdbd808b1303157495b84cef5cdee7ba1f529
AlexisDerail <alexis.derail@etu.minesparis.psl.eu>
2025-01-31 11:41:45 +0100

    alexis

--- alexisderail.py
+++ alexisderail.py
@@ -1,44 +1,47 @@
+
 import arcade
-#import random as rd
+import random as rd
 
+BACKGROUND = arcade.color.ALMOND
 WIDTH = 800
-HEIGHT = 800
+HEIGHT = 600
 TITLE = "Worlds of Goo"
-#PLATEFORME = "\data\plateform.png"
-#BACKGROUND = "\prout"
+PLATEFORME = "data/plateforme3.png"
+Spread = 0.1
 
-
-"""
-class Plateform(arcade.Window):
+class Plateform(arcade.Sprite):
 
     def __init__(self, n):
         super().__init__(PLATEFORME)
         if n == 0:
-            self.center_x = (WIDTH / 4) * (1+rd.normal(-0.5, 0.5))
+            self.center_x = (WIDTH / 4) * (1+rd.uniform(-Spread, Spread))
         else : 
-            self.center_x = (3 * WIDTH / 4) * (1+rd.normal(-0.5, 0.5))
-        self.center_y = (HEIGHT / 2)*(1+rd.normal(-0.5, 0.5))
-"""
+            self.center_x = (3 * WIDTH / 4) * (1+rd.uniform(-Spread, Spread))
+        self.center_y = (HEIGHT / 2)*(1+rd.uniform(-Spread, Spread))
+
 
 class Window(arcade.Window):
 
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, TITLE)
+        arcade.set_background_color(BACKGROUND)
         #arcade.set_background_color(BACKGROUND)
-        #self.set_location(800, 100)
-        #self.plateformes = arcade.SpriteList()
+        self.set_location(800, 100)
+        self.plateforms = arcade.SpriteList()
     
-    #def setup():
-    #    for n in range(0, 1):
-    #        plateforme = Plateform(n)
+    def setup(self):
+        for n in range(0, 2):
+            plateform = Plateform(n)
+            self.plateforms.append(plateform)
+
+    def on_update(self, delta_time):
+        pass
 
     def on_draw(self):
-        arcade.start_render()
-        #self.plateformes.draw()
+        #arcade.start_render()
+        self.plateforms.draw()
 
-def main():
-    window = Window()
-    arcade.run()  # Lance la boucle de jeu, elle attend les événements et affiche la fenêtre
+window = Window()
+window.setup()
+arcade.run()
 
-if __name__ == "__main__":
-    main()
