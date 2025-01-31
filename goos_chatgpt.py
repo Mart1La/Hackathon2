edb051a9e6be2200d7394c6b862e4fdab5d59591
Paul Delort <paul.delort@etu.minesparis.psl.eu>
2025-01-31 16:02:52 +0100

    g_fonctionne_1

--- goos_chatgpt.py
+++ goos_chatgpt.py
@@ -13,8 +13,8 @@ CRITICAL_DISTANCE = 300     # Distance à partir de laquelle on considère les a
 TITLE = "Worlds of Goo"
 PLATEFORME = "data/plateforme3.png"
 Spread = 0.1
-g = 0.495
-k = 100
+g = 9.81 * 10
+k = 0.5
 m = 0.4
 
 LINK = "data/link.png"
@@ -26,6 +26,7 @@ class Goo(arcade.Sprite):
     def __init__(self, x, y):
         super().__init__(GOO)
         self.center_x, self.center_y = x, y
+        self.center_x_previous, self.center_y_previous = x, y
         self.angle = 0
 
 # class Link(arcade.Sprite):
@@ -50,7 +51,7 @@ class Window(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, LENGTH, TITLE)
         arcade.set_background_color(BACKGROUND)
-        self.set_location(400, 100)
+        self.set_location(100, 30)
         self.plateforms = arcade.SpriteList()
         # self.links = arcade.SpriteList()
         self.Goos = arcade.SpriteList()
@@ -85,23 +86,26 @@ class Window(arcade.Window):
                 self.Goos_adj[len(self.Goos) - 1].append((i, dist))
 
     def on_update(self, delta_time):
+        DELTA_TIME = delta_time
+
         indices = list(range(len(self.Goos)))
         rd.shuffle(indices)
 
         for ind in indices:
-            prev_pos_x, prev_pos_y = self.Goos[ind].center_x, self.Goos[ind].center_y  # Store previous position for velocity calculations
+            current_goo = self.Goos[ind]           
             acc = g*np.array([0, -1])
             for duo in self.Goos_adj[ind]:
                 acc += (k/m) * np.array(
-                    (np.sqrt((self.Goos[ind].center_y)**2 + (self.Goos[ind].center_x)**2) - np.sqrt((self.Goos[duo[0]].center_y)**2 + (self.Goos[duo[0]].center_x)**2) - duo[1])
-                    * np.sin(np.atan2((self.Goos[duo[0]].center_x - self.Goos[ind].center_x) , (self.Goos[ind].center_y - self.Goos[duo[0]].center_y))) ,
-                    (np.sqrt((self.Goos[ind].center_y)**2 + (self.Goos[ind].center_x)**2) - np.sqrt((self.Goos[duo[0]].center_y)**2 + (self.Goos[duo[0]].center_x)**2) - duo[1])
-                    * np.cos(np.atan2((self.Goos[duo[0]].center_x - self.Goos[ind].center_x) , (self.Goos[ind].center_y - self.Goos[duo[0]].center_y)))
+                    (np.sqrt((current_goo.center_y)**2 + (current_goo.center_x)**2) - np.sqrt((self.Goos[duo[0]].center_y)**2 + (self.Goos[duo[0]].center_x)**2) - duo[1])
+                    * np.sin(np.atan2((self.Goos[duo[0]].center_x - current_goo.center_x) , (current_goo.center_y - self.Goos[duo[0]].center_y))),
+                    (np.sqrt((current_goo.center_y)**2 + (current_goo.center_x)**2) - np.sqrt((self.Goos[duo[0]].center_y)**2 + (self.Goos[duo[0]].center_x)**2) - duo[1])
+                    * np.cos(np.atan2((self.Goos[duo[0]].center_x - current_goo.center_x) , (current_goo.center_y - self.Goos[duo[0]].center_y)))
                 )
-            newx_tdt = 2*self.Goos[ind].center_x - prev_pos_x + acc[0]*(delta_time)**2
-            newy_tdt = 2*self.Goos[ind].center_y - prev_pos_y + acc[1]*(delta_time)**2
-            self.Goos[ind].center_x = int(newx_tdt)
-            self.Goos[ind].center_y = int(newy_tdt)
+            newx_tdt = 2*current_goo.center_x - current_goo.center_x_previous + acc[0]*(DELTA_TIME)**2
+            newy_tdt = 2*current_goo.center_y - current_goo.center_y_previous + acc[1]*(DELTA_TIME)**2
+            current_goo.center_x_previous, current_goo.center_x = current_goo.center_x, newx_tdt
+            current_goo.center_y_previous, current_goo.center_y = current_goo.center_y, newy_tdt
+            print(newy_tdt - current_goo.center_y_previous)
 
 # Lancement du jeu
 window = Window()
